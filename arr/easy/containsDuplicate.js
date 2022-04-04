0101e8e9a67e57b97f69ec8c9b4bcf7a9354849f
Yasir <hasanyasir01@gmail.com>
2022-04-04 16:55:55 -0700

    containsDuplicates()

---
+++ arr/easy/containsDuplicate.js
@@ -0,0 +1,23 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+function containsDuplicates(nums) {
+  return new Set(nums).size < nums.length;
+}
+
+function containsDuplicates_1(nums) {
+  let set = new Set();
+  for (const num of nums) {
+    if (set.has(num)) return true;
+    set.add(num);
+  }
+  return false;
+}
+
+function containsDuplicates_2(nums) {
+  let hashTable = {};
+  for (const num of nums) {
+    if (hashTable[num]) return true;
+    hashTable[num] = true;
+  }
+  return false;
+}
