9cc8b303541fdbcbfe863e72fcc749828f12b0f5
Yasir <hasanyasir01@gmail.com>
2022-05-07 16:02:24 -0700

    defector the function

--- arr/easy/searchBS.test.js
+++ arr/easy/searchBS.test.js
@@ -1,19 +1,23 @@
 // find a number in sorted array of numbers
 // The idea is to search a value in a sorted array
 function search(numbers, target) {
-  let index = Math.ceil((numbers.length - 1) / 2);
+  let index = mid(numbers.length - 1);
   let [left, right] = [0, numbers.length - 1];
   while (left <= right) {
     if (numbers[index] === target) return index;
     else if (target > numbers[index]) {
       left = index + 1;
-      index = Math.ceil((left + right) / 2);
+      index = mid(left + right);
     } else {
       right = index - 1;
-      index = Math.ceil((left + right) / 2);
+      index = mid(left + right);
     }
   }
   return -1;
+}
+
+function mid(num1 = 0, num2 = 0) {
+  return Math.ceil((num1 + num2) / 2);
 }
 describe("search()", () => {
   it("Function should use a binary search to find a target in a sorted array, if not found it return -1", () => {
